7fdbcdf9e72254b017b1d8b4497edc174acb403e
ladaka.tayaara <abhiventor@gmail.com>
2025-07-10 21:12:41 +0530

    powerrec

---
+++ recursion/power.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int power(int m,int n){
+    if(n==0){
+        return 1;
+    }
+    return power(m,n-1)*m;
+}
+
+int power1(int m,int n){
+    if(n==0){
+        return 1;
+    }
+    if(n%2==0){
+        return power1(m*m,n/2);
+    }
+    return m*power(m*m,(n-1)/2);
+}
+
+int main(){
+    int r=power(6,2);
+    int n=power1(5,2);
+    cout<<r<<endl<<n;
+
+}

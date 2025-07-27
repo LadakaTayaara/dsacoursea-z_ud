d475a3e377e63bbbb16de4c955c9253346a5bbbe
ladaka.tayaara <abhiventor@gmail.com>
2025-07-27 11:45:44 +0530

    minor changes

--- recursion/expon.cpp
+++ recursion/expon.cpp
@@ -1,8 +1,15 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int exp(int m,int n){
     if (n==0){
         return 1;
     }
     return exp(m,n-1)*m;
+}
+
+int main(){
+    int a=exp(5,3);
+    cout<<a<<"\n";
+    return 0;
 }

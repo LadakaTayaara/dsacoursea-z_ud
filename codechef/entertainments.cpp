0bc9591444c8b37aacbc618d74e11ef19e8d71d5
ladaka.tayaara <abhiventor@gmail.com>
2025-12-18 22:52:20 +0530

    solved 3/4 of attempted , a starter contest

---
+++ codechef/entertainments.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n>0){
+	    if(n<5){
+	        cout<<n*200;
+	    }
+	    else{
+	        cout<<1000;
+	    }
+	}
+}

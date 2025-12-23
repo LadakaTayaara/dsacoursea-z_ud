375c1c85c9e7f2b35e79d25b4bb62107c694b3b7
ladaka.tayaara <abhiventor@gmail.com>
2025-12-23 17:19:30 +0530

    modifications

--- dynamicprogramming/ninjatraining.cpp
+++ dynamicprogramming/ninjatraining.cpp
@@ -6,15 +6,6 @@ int nin(int day,int last,vector<vector<int>> &points, vector<vector<int>> &dp){
     if(dp[day][last]!=-1){
         return dp[day][last];
     }
-    if(day==0){
-        int maxi=0;
-        for(int task=0;task<3;task++){
-            if(task!=last){
-                maxi = max(maxi, points[0][task]);
-            }
-        }
-        return dp[0][last]=maxi;
-    }
     int maxi=0;
     for(int task=0;task<3;task++){
             if(task!=last){
@@ -25,6 +16,26 @@ int nin(int day,int last,vector<vector<int>> &points, vector<vector<int>> &dp){
     return dp[day][last]=maxi;
 }
 
-int main(){
-    
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<vector <int>> dp(n,vector <int>(4,-1));
+    dp[0][0]=max(points[0][1],points[0][2]);
+    dp[0][1]=max(points[0][0],points[0][2]);
+    dp[0][2]=max(points[0][1],points[0][1]);
+    dp[0][3]=max(points[0][1],points[0][1],points[0][2]);
+
+    for(int day=1;day<n;day++){
+        for(int last=0;last<4;last++){
+            int maxi=0;
+            for(int task=0;task<3;task++){
+                if(task!=last){
+                    int point=points[day][task]+dp[day-1][task];
+                    maxi = max(maxi, point);
+                }
+    }
+    dp[day][last]=maxi;
+        }
+    }
+
+    return dp[n-1][3];
 }

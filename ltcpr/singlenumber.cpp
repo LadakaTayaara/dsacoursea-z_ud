551cd69a8c2c92c8dad7b895ea525e01bf17edde
ladaka.tayaara <abhiventor@gmail.com>
2025-12-24 23:03:38 +0530

    modifications

--- ltcpr/singlenumber.cpp
+++ ltcpr/singlenumber.cpp
@@ -5,22 +5,10 @@ using namespace std;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int mx=0;
+        int res=0;
         int len=nums.size();
         for(int i=0;i<len;i++){
-            if(nums[i]>mx){
-                mx=nums[i];
-            }
-        }
-        vector<int> hsh(mx+1);
-        for(int i=0;i<len;i++){
-            hsh[nums[i]]++;
-        }
-        int res;
-        for(int i=0;i<=mx;i++){
-            if(hsh[i]==1){
-                res = i;
-            }
+            res ^= nums[i];
         }
         return res;
     }

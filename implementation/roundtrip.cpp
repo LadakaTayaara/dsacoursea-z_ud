10feae528546b1f35c81f5e6547fb48a14f93004
ladaka.tayaara <abhiventor@gmail.com>
2025-12-27 22:53:24 +0530

    round trip problem codeforces

--- implementation/roundtrip.cpp
+++ implementation/roundtrip.cpp
@@ -15,25 +15,19 @@ using vll = vector<ll>;
 #define rall(x) (x).rbegin(), (x).rend()
 
 void solve() {
-    long long r,x,d,n;
-    cin >>r>>x>>d>>n;
-    string s(n,'*');
-    cin>>s;
-    int res=0;
-    for(long long i=0;i<n;i++){
-        if((s[i]=='1') || (s[i]=='2' && r<x)){
-            if(r-d>=0){
-                r-=d;
-            }
-            else if(r==0){
-                r=0;
-            }
+    long long r, x, d, n;
+    cin >> r >> x >> d >> n;
+    string s;
+    cin >> s;
+    int res = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(s[i] == '1' || (s[i] == '2' && r < x)) {
             res++;
+            r = max(0LL, r - d); 
         }
     }
-
-    cout<<res<<endl;
-    
+    cout << res << endl;
 }
 
 int main() {

44e61d90ad48f56b71d862836f0890d22a0b8ee3
ladaka.tayaara <abhiventor@gmail.com>
2025-12-29 11:40:58 +0530

    wasted unnecessary time on a one liner

---
+++ upsolving/tabclosing.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+// Type aliases
+using ll = long long;
+using pii = pair<int,int>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+// Macros
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+void solve() {
+    int a,b,n;
+    cin >>a>>b>>n;
+    cout << ( (a==b || (long long)b*n<=a) ? "1" : "2" ) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

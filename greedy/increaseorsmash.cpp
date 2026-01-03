19ed7a292b0a5dfa4695065b7835d83d091a1748
ladaka.tayaara <abhiventor@gmail.com>
2026-01-03 16:52:58 +0530

    increase or smash problem, done via solution video , implemented by my own

---
+++ greedy/increaseorsmash.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type aliases
+using ll = long long;
+using pii = pair<int,int>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+// Macros
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+void solve() {
+    int n;
+    cin >> n;
+    set<int> s;
+    for(int i=0;i<n;i++){
+        int ip;
+        cin>>ip;
+        s.insert(ip);
+    }
+    int ans=2*(s.size())-1;
+    cout<<ans<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

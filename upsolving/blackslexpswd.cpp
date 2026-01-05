90d57ed8c9667672c63bfa4ba28dd7e877bf719a
ladaka.tayaara <abhiventor@gmail.com>
2026-01-05 17:31:09 +0530

    div 3 A problem CF

---
+++ upsolving/blackslexpswd.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type aliases
+using ll = long long;
+using pii = pair<int,int>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+// Macros
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+void solve() {
+    int k,x;
+    cin >> k>>x;
+    cout<<k*x+1<<endl;;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

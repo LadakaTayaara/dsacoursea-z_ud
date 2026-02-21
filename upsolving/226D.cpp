4a7d9bee6eaa354b24feb348cae57e4968416acc
ladaka.tayaara <abhiventor@gmail.com>
2026-02-22 00:40:27 +0530

    need to learn maps

---
+++ upsolving/226D.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type aliases
+using ll = long long;
+using pii = pair<int,int>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+// Macros
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    map<int, int> counts;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        counts[a[i]]++;
+    }
+
+    int first_val = a[0];
+    int initial_distinct = counts.size();
+    
+    vector<int> freqs;
+    for (map<int, int>::iterator it = counts.begin(); it != counts.end(); ++it) {
+        if (it->first == first_val) continue;
+        freqs.push_back(it->second);
+    }
+
+    sort(freqs.begin(), freqs.end());
+
+    int eliminated = 0;
+    for (int f : freqs) {
+        if (k >= f) {
+            k -= f;
+            eliminated++;
+        } else {
+            break;
+        }
+    }
+
+    cout << initial_distinct - eliminated << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
